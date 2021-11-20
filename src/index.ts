46839cbee173b9ae2455d8fb301ba9d39c500af1
Patrick Lee Scott <pat@patscott.io>
2021-11-19 21:49:22 -0600

    feat: respond to / with 200

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,10 @@ app.use('/api', services);
 
 const port = process.env.PORT || 8000;
 
+app.get('/', (req, res) => {
+	res.send('ok');
+})
+
 app.listen(port, () =>
 	console.log(`Express app listening on localhost:${port}`)
 );
