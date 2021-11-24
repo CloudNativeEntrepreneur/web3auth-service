0ffb19bfd2995b69eb0001cd93077f1d34710c07
Patrick Lee Scott <pat@patscott.io>
2021-11-23 18:51:27 -0600

    feat: basic auth for unprotected routes using clientId/clientSecret
    fix: format

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,16 @@
 import os from "os";
 import path from "path";
 
+const clientId = process.env.CLIENT_ID || "web3-auth-client";
+const clientSecret =
+  process.env.CLIENT_SECRET ||
+  "replace me with random characters, like a guid, or a few, at least 32 characters";
+
 export const config = {
+  clientId,
   jwt: {
     algorithms: ["HS256" as const],
-    secret:
-      process.env.CLIENT_SECRET ||
-      "replace me with random characters, like a guid, or a few, at least 32 characters", // TODO Put in process.env
+    secret: clientSecret,
   },
   publicURI: process.env.PUBLIC_URI || "http://localhost:8000/",
   database: {
--- src/services/users/routes.ts
+++ src/services/users/routes.ts
@@ -1,19 +1,30 @@
 import express from "express";
 import jwt from "express-jwt";
+import basicAuth from "express-basic-auth";
 import { config } from "../../config";
 import * as controller from "./controller";
 
 export const userRouter = express.Router();
 
 /** GET /api/users */
-userRouter.route("/").get(controller.find);
+userRouter.route("/").get(
+  basicAuth({
+    users: { [config.clientId]: config.jwt.secret },
+  }),
+  controller.find
+);
 
 /** GET /api/users/:publicAddress */
 /** Authenticated route */
 userRouter.route("/:publicAddress").get(jwt(config.jwt), controller.get);
 
 /** POST /api/users */
-userRouter.route("/").post(controller.create);
+userRouter.route("/").post(
+  basicAuth({
+    users: { [config.clientId]: config.jwt.secret },
+  }),
+  controller.create
+);
 
 /** PATCH /api/users/:publicAddress */
 /** Authenticated route */
